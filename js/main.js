eb809d2d41a0afdfe4bc6706a797c05bcffe525f
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-01-23 17:59:12 -0300

    refatorando função de adição de valores

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-document.getElementById('robotron').addEventListener('click', dizOi )
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.getElementById('braco');
 
-function dizOi(){
-    console.log('oi')
+const controle = document.querySelectorAll('.controle-ajuste');
+
+
+controle.forEach((elemento) => {
+    elemento.addEventListener('click',(evento) => {
+        manipulaDados(evento.target.textContent);
+        console.log(evento);
+    })
+})
+
+function manipulaDados(operacao){
+    if(operacao ==="+"){
+        braco.value = parseInt(braco.value) + 1;
+    } else{
+        braco.value = parseInt(braco.value) - 1; 
+    }
 }
